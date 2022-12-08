d8e03d6b94618a7fcd14c167260364d2e14080f9
MD-Baki <bakimd77@gmail.com>
2022-12-08 12:47:31 +0600

    loader

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,13 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Headers/Navbar";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Loader from "../components/Loader/Loader";
 
 const Main = () => {
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         AOS.init();
     }, []);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);
+
+    if (loading) return <Loader />;
+
     return (
         <div>
             <Navbar />
